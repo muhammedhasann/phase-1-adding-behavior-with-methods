2185622b04b807c5821d7307fb695b85bfc2e57a
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-04-02 19:21:44 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Cat {
+    constructor(name,sex) { 
+    this.name = name;
+    this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+} 
+const cat = new Cat("Sasha", "female")
+
+class Dog {
+    constructor(name,sex) { 
+    this.name = name;
+    this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+const dog = new Dog("Rufio", "male")
+class Bird {
+        constructor(name,sex) { 
+    this.name = name;
+    this.sex = sex;
+    }
+    speak() {
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+const  bird = new Bird("Pablo", "male")
+
+const bird2 = new Bird("Mable", "female")
